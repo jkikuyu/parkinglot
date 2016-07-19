ebad308900c1c07e7cc2169530f0656a7eccf9d5
Jude <jkikuyu@gmail.com>
2016-07-19 23:07:20 +0300

    added User.java

---
+++ src/com/parkinglot/User.java
@@ -0,0 +1,214 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.parkinglot;
+
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+/**
+ *
+ * @author Jude Kikuyu
+ */
+@Entity
+@Table(name = "USER", catalog = "PUBLIC", schema = "PUBLIC")
+@NamedQueries({
+    @NamedQuery(name = "User.findAll", query = "SELECT u FROM User u"),
+    @NamedQuery(name = "User.findByid", query = "SELECT u FROM User u WHERE u.id = :id")
+})
+public class User implements Serializable {
+    @Transient
+    private PropertyChangeSupport changeSupport = new PropertyChangeSupport(this);
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "FNAME")
+    private String fname;
+    @Column(name = "LNAME")
+    private String lname;
+    @Column(name = "USERNAME")
+    private String username;
+    @Column(name = "PASSWORD")
+    private String password;
+    @Column(name = "ACTIVE")
+    private Boolean active;
+    @Column(name = "EMAIL")
+    private String email;
+    @Column(name = "ADDRESS")
+    private String address;
+    @Column(name = "POSTALCODE")
+    private String postalcode;
+    @Column(name = "TOWN")
+    private String town;
+    @Column(name = "CREATEDON")
+    private String createdon;
+    @Column(name = "CREATEDBY")
+    private String createdby;
+    public User(){
+    }
+
+
+    public User(Integer id) {
+        this.id = id;
+    }
+
+    public User(Integer id, String fname) {
+        this.id = id;
+        this.fname = fname;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer userid) {
+        Integer oldUserid = this.id;
+        this.id = userid;
+        changeSupport.firePropertyChange("id", oldUserid, userid);
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        String oldFname = this.fname;
+        this.fname = fname;
+        changeSupport.firePropertyChange("fname", oldFname, fname);
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        String oldLname = this.lname;
+        this.lname = lname;
+        changeSupport.firePropertyChange("lname", oldLname, lname);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        String oldUsername = this.username;
+        this.username = username;
+        changeSupport.firePropertyChange("username", oldUsername, username);
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        String oldPassword = this.password;
+        this.password = password;
+        changeSupport.firePropertyChange("password", oldPassword, password);
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        Boolean oldActive = this.active;
+        this.active = active;
+        changeSupport.firePropertyChange("active", oldActive, active);
+    }
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPostalcode() {
+        return postalcode;
+    }
+
+    public void setPostalcode(String postalcode) {
+        this.postalcode = postalcode;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+    public String getCreatedby() {
+        return createdby;
+    }
+
+    public void setCreatedby(String createdby) {
+        this.createdby = createdby;
+    }
+
+    public String getCreatedon() {
+        return createdon;
+    }
+
+    public void setCreatedon(String createdon) {
+        this.createdon = createdon;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof User)) {
+            return false;
+        }
+        User other = (User) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "parkinglotview.Users[userid=" + id + "]";
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.removePropertyChangeListener(listener);
+    }
+
+}
